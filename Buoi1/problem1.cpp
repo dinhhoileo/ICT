2e7e6952c0c4eb873bd431a9ab4061596ea81c57
dinhhoileo <dinhhoi2004@gmail.com>
2023-04-16 22:12:31 +0700

    update

--- Buoi1/problem1.cpp
+++ Buoi1/problem1.cpp
@@ -1,29 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 #define MAX 100
-void sumTarget(int arr[MAX],int size,int target)
+void sumTarget(int arr[MAX], int size, int target)
 {
-    for (int i = 0; i < size-1; i++)
+    for (int i = 0; i < size - 1; i++)
     {
-        for (int j = i+1; j < size; j++)
+        for (int j = i + 1; j < size; j++)
         {
-            if (arr[i]+arr[j]==target)
+            if (arr[i] + arr[j] == target)
             {
-                cout<<i<<"\t"<<j<<endl;
+                cout << i << "\t" << j << endl;
             }
-            
         }
-        
     }
-    
 }
 
 int main()
 {
-    int arr[]={3,2,1,5,4,6,8};
-    int target=9;
-    int size=sizeof(arr)/sizeof(arr[0]);
-    sumTarget(arr,size,9);    
+    int arr[] = {3, 2, 1, 5, 4, 6, 8};
+    int target = 9;
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sumTarget(arr, size, 9);
 
     return 0;
 }
--- Buoi1/problem2.cpp
+++ Buoi1/problem2.cpp
@@ -1,35 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 #define MAX 100
 void bubleSort(int arr[MAX], int size)
 {
-    for (int i = 0; i < size-1; i++)
+    for (int i = 0; i < size - 1; i++)
     {
-        for (int j =i+1; j < size; j++)
+        for (int j = i + 1; j < size; j++)
         {
-            if (arr[i]>arr[j])
+            if (arr[i] > arr[j])
             {
-                swap(arr[i],arr[j]);
+                swap(arr[i], arr[j]);
             }
-            
         }
-        
     }
-    
 }
-void outputArray(int arr[MAX],int size)
+void outputArray(int arr[MAX], int size)
 {
     for (int i = 0; i < size; i++)
     {
-        cout<<"\t"<<arr[i];
+        cout << "\t" << arr[i];
     }
-    
 }
 int main()
 {
-    int arr[]={1,4,2,3,6,5,8};
-    int size=sizeof(arr)/sizeof(arr[0]);
-    bubleSort(arr,size);
-    outputArray(arr,size);
+    int arr[] = {1, 4, 2, 3, 6, 5, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    bubleSort(arr, size);
+    outputArray(arr, size);
     return 0;
 }
